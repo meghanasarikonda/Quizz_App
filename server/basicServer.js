4e5fcb4d96b7af1c2e8357b00196ea4998283e2e
meghanasarikonda <meghana94987@gmail.com>
2017-10-03 04:43:46 +0530

    Server listening on port 3000

--- server/basicServer.js
+++ server/basicServer.js
@@ -2,8 +2,8 @@ var http = require('http');
 var express = require('express')
 var app = express();
 
-app.get('/', function(req, res) {
-  res.end('hello, world')
-}).listen(3000, function() {
+app.use(express.static('compiled'));
+
+app.listen(3000, function() {
   console.log('listening on port 3000');
 });
