23d33a9c2d42028201c9d53094286eb283788bbe
meghanasarikonda <meghana94987@gmail.com>
2017-10-05 22:36:01 +0530

    Able to display correct or wrong answer upon submission

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -1,29 +1,89 @@
-class Question extends React.Component{
+class Question extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {selectedOption: 'a'}
+    this.state = {
+      selectedOption: '',
+      correctOption: this.props.singleQuestion.ans
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOptionChange = this.handleOptionChange.bind(this);
   }
+
+  handleSubmit(submitEvent) {
+    submitEvent.preventDefault();
+    if (this.state.correctOption === this.state.selectedOption)
+      alert('Correct answer!');
+    else
+    alert('Wrong answer!');
+  }
+
+  handleOptionChange(changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+
+  }
+
+
+
   render() {
-    // console.log(this.props, 'props')
-    // const { singleQuestion } = this.props
-    const singleQuestion = this.props.singleQuestion
+    // console.log('correctOption', this.state.correctOption)
+    // console.log('You have selected:', this.state.selectedOption);
+    const singleQuestion = this.props.singleQuestion;
     return (
       <div>
-        <li>{ singleQuestion.q }</li>
-        <form onSubmit={(event) => {
-          console.log(event)
-          event.preventDefault()
-          }}>
-          <input onChange={(e) => console.log(e.target.value)} type="radio" value="a" checked={this.state.selectedOption === 'a'} /> {singleQuestion.a}
-          <input onChange={(e) => console.log(e.target.value)} type="radio" value="b" checked={this.state.selectedOption === 'b'} /> {singleQuestion.b}
-          <input onChange={(e) => console.log(e.target.value)} type="radio" value="c" checked={this.state.selectedOption === 'c'} /> {singleQuestion.c}
-          <input onChange={(e) => console.log(e.target.value)} type="radio" value="d" checked={this.state.selectedOption === 'd'} /> {singleQuestion.d}
-          <input type="submit" />
+        <form>
+          {singleQuestion.q}
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value="a"
+                checked={this.state.selectedOption === 'a'}
+                onChange={this.handleOptionChange}
+              />
+              {singleQuestion.a}
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value="b"
+                checked={this.state.selectedOption === 'b'}
+                onChange={this.handleOptionChange}
+              />
+              {singleQuestion.b}
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value="c"
+                checked={this.state.selectedOption === 'c'}
+                onChange={this.handleOptionChange}
+              />
+              {singleQuestion.c}
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value="d"
+                checked={this.state.selectedOption === 'd'}
+                onChange={this.handleOptionChange}
+              />
+              {singleQuestion.d}
+            </label>
+          </div>
+          <input
+            type="submit"
+            onClick={this.handleSubmit}
+          />
         </form>
-
       </div>
-    )
+    );
   }
 }
-
-window.Question = Question;
