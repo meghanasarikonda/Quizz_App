8a937c154b0591c2be89867757ab750702d9fdfe
meghanasarikonda <meghana94987@gmail.com>
2017-10-07 02:11:57 +0530

    Set foreignKey of user_id to Quiz table

--- server/basicServer.js
+++ server/basicServer.js
@@ -2,7 +2,7 @@ var http = require('http');
 var bodyParser = require('body-parser');
 var express = require('express')
 var app = express();
-var Quiz = require('./db/index')
+var db = require('./db/index')
 
 app.use(bodyParser.json());
 
@@ -16,35 +16,46 @@ app.use(express.static('compiled'));
 
 app.get('/api/quizzes', function(req, res) {
   // console.log('in serevr',req.query)
-  Quiz.Quiz.findAll()
+  db.Quiz.findAll()
   .then(quizzes => {
     res.send(quizzes);
     // return users;
   })
 })
 
-app.get('/api/usescores', function(req, res) {
+app.get('/api/userscores', function(req, res) {
 
 })
 
 
-app.post('/api/quizdata', function(req, res) {
-  // console.log('POST /api/quizdata req', req.body)
+app.post('/api/quizdata', function(req, response) {
+  console.log('POST /api/quizdata req', req.body)
 
-  Quiz.Quiz.create({
-    q: req.body.question,
-    a: req.body.a,
-    b: req.body.b,
-    c: req.body.c,
-    d: req.body.d,
-    ans: req.body.answer
+
+  db.User.create({
+    username: req.body.username
   })
   .then(res => {
-    res.send('postedSuccessfully');
+    // console.log('---->', res)
+    db.Quiz.create({
+      user_id: res.dataValues.user_id,
+      q: req.body.question,
+      a: req.body.a,
+      b: req.body.b,
+      c: req.body.c,
+      d: req.body.d,
+      ans: req.body.answer
+    })
+    .then(res => {
+      response.send('postedSuccessfully');
+    })
+    .catch(err => {
+      console.log('error in creating Quiz', err)
+    })
   })
   .catch(err => {
-    res.status(500)
-    res.send('postFailed')
+    response.status(500)
+    response.send('postFailed')
     console.log(err);
   })
 
