227e448c2bc7a03d27df64e87908c36fd834f5be
meghanasarikonda <meghana94987@gmail.com>
2017-10-06 09:04:42 +0530

    Create User, User_Score models

--- server/db/index.js
+++ server/db/index.js
@@ -11,21 +11,43 @@ var db = new Sequelize('quiz', 'root', '', {
   }
 });
 
-var Quiz = db.define('Quiz', {
+
+
+var User = db.define('user', {
+  username: Sequelize.STRING
+});
+
+var Quiz = db.define('quiz', {
   q: Sequelize.STRING,
   a: Sequelize.STRING,
   b: Sequelize.STRING,
   c: Sequelize.STRING,
   d: Sequelize.STRING,
   ans: Sequelize.TEXT
+});
 
+var User_Score = db.define('user_score', {
+  test_score: Sequelize.INTEGER
 });
 
 // force: true will drop the table if it already exists
 
+// describe the model Quiz associations
+Quiz.belongsTo(User);
+User.hasMany(Quiz);
 
-Quiz.sync()
+// describe the model User_Score assoc...
+User.hasMany(User_Score);
+User_Score.belongsTo(User);
+Quiz.hasMany(User_Score);
+User_Score.belongsTo(Quiz);
 
 
+db.sync({
+  //force: true
+})
 
+
+exports.User = User;
 exports.Quiz = Quiz;
+exports.User_Score = User_Score;
