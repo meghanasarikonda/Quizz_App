18121791b58b90887c3d1228bc076343ea041d2f
meghanasarikonda <meghana94987@gmail.com>
2017-10-06 21:38:57 +0530

    Add hidden input tag

--- client/components/CreateQuiz.jsx
+++ client/components/CreateQuiz.jsx
@@ -3,6 +3,19 @@ class CreateQuiz extends React.Component {
     super();
   }
 
+  // componentDidMount () {
+  //   console.log(this, 'this.props')
+  //   axios.post('/api/username', {
+  //     username: this.props.username
+  //   })
+  //   .then((res) => {
+  //     console.log(res, 'resFromCreateQuizComp')
+  //   })
+  //   .catch(err => {
+  //     console.log('error')
+  //   })
+  // };
+
   render() {
     return (
       <div>
@@ -10,11 +23,12 @@ class CreateQuiz extends React.Component {
         <p>Wanna Create a Quizz?</p>
         <form action="http://localhost:3000/api/quizdata" method="post">
           <input type="text" placeholder="Enter the question" name="question"/>
-          <input type="text" placeholder="Enter option 1" name="a"/>
-          <input type="text" placeholder="Enter option 2" name="b"/>
-          <input type="text" placeholder="Enter option 3" name="c"/>
-          <input type="text" placeholder="Enter option 4" name="d"/>
+          <input type="text" placeholder="Enter option a" name="a"/>
+          <input type="text" placeholder="Enter option b" name="b"/>
+          <input type="text" placeholder="Enter option c" name="c"/>
+          <input type="text" placeholder="Enter option d" name="d"/>
           <input type="text" placeholder="Enter correct option" name="answer"/>
+          <input type="hidden" name="username" value={this.props.username} />
           <input type="submit"/>
         </form>
       </div>
